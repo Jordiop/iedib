8a996449ba254fd0b3023dbf93fd259410bd0927
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2024-12-15 16:44:00 +0100

    feat: big data aplicat

--- Big data Aplicat/Practica 3/Apartat 1/actividad1.sql
+++ Big data Aplicat/Practica 3/Apartat 1/actividad1.sql
@@ -41,18 +41,25 @@ LOAD DATA LOCAL INPATH "/home/cloudera/Desktop/results.csv" INTO TABLE soccer.re
 -- Consultes
 --1
 SELECT count(*) FROM soccer.goalscorers WHERE scorer = 'Lionel Messi' AND own_goal = FALSE;
+-- 55
 
 --2
 SELECT * FROM soccer.results
 WHERE home_team = 'Spain' OR away_team = 'Spain'
 ORDER BY date DESC
 LIMIT 5;
-
+/* 2024-11-18	Spain	Switzerland	3	2	UEFA Nations League	Santa Cruz de Tenerife	Spain		
+2024-11-15	Denmark	Spain	1	2	UEFA Nations League	Copenhagen	Denmark		
+2024-10-15	Spain	Serbia	3	0	UEFA Nations League	Cordoba	Spain		
+2024-10-12	Spain	Denmark	1	0	UEFA Nations League	Murcia	Spain		
+2024-09-08	Switzerland	Spain	1	4	UEFA Nations League	Geneva	Switzerland		 */
+  
 --3
 SELECT SUM
 (CASE WHEN home_team = 'Spain' THEN home_score ELSE 0 END + 
 CASE WHEN away_team = 'Spain' THEN away_score ELSE 0 END)
 FROM soccer.results;
+--1553
 
 --4
 SELECT scorer, count(*) as goals
@@ -61,6 +68,11 @@ WHERE team = 'Spain' AND own_goal = FALSE
 GROUP BY scorer
 ORDER BY goals DESC
 LIMIT 5;
+/* David Villa	41	
+Raúl	32	
+Álvaro Morata	29	
+Fernando Torres	28	
+Fernando Hierro	25	 */
 
 --5
 SELECT scorer
@@ -69,12 +81,23 @@ INNER JOIN soccer.results ON goalscorers.date = results.date
 WHERE team = 'Spain' AND penalty = TRUE AND tournament = 'UEFA Euro'
 GROUP BY scorer
 ORDER BY scorer;
+/* Daniel Ruiz	
+Francisco José Carrasco	
+Gaizka Mendieta	
+Xabi Alonso	 */
+
 
 --6
 SELECT scorer, count(*) as goals
 FROM soccer.goalscorers
 INNER JOIN soccer.results ON goalscorers.date = results.date
-WHERE competition LIKE '%FIFA World Cup%' AND own_goal = FALSE
+WHERE tournament = 'FIFA World Cup' AND own_goal = FALSE
 GROUP BY scorer
 ORDER BY goals DESC
 LIMIT 5;
+/* Just Fontaine	60	
+Gerd Müller	49	
+Helmut Rahn	44	
+Uwe Seeler	44	
+Miroslav Klose	43	
+ */
--- Big data Aplicat/Practica 3/Apartat 2/actividad2.sql
+++ Big data Aplicat/Practica 3/Apartat 2/actividad2.sql
@@ -27,13 +27,19 @@ WHERE type = 'movie'
 GROUP BY platform
 ORDER BY num_movies DESC
 LIMIT 1;
+-- amazon	59244	
 
 -- 2
 SELECT title, imdbAverageRating, platform
 FROM streaming.movies
-WHERE type = 'series'
+WHERE type = 'tv'
 ORDER BY imdbAverageRating DESC
 LIMIT 5;
+/* Benjamin Cello	9.5	amazon	
+Breaking Bad	9.5	netflix	
+Band of Brothers	9.3999996185302734	netflix	
+Getting Dirty in Japan	9.3999996185302734	amazon	
+Life After	9.3999996185302734	amazon	 */
 
 -- 3
 SELECT platform, SUM(imdbNumVotes) AS total_votes
@@ -43,6 +49,11 @@ WHERE genres LIKE '%Science Fiction%'
    AND type = 'tv'
 GROUP BY platform
 ORDER BY total_votes DESC;
+/* amazon	3403767	
+apple	2375367	
+netflix	1665835	
+hulu	1551810	
+hbo	745275	 */
 
 -- 4
 SELECT releaseYear, count(*) as num_movies
@@ -51,3 +62,8 @@ WHERE type = 'movie'
 GROUP BY releaseYear
 ORDER BY num_movies DESC
 LIMIT 5;
+/* 2022	6068	
+2019	6045	
+2018	5711	
+2021	5522	
+2017	5209	 */
